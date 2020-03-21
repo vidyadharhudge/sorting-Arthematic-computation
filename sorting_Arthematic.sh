67b15424857c2955d6f914b69e469f72ad0de3fc
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-21 10:04:21 +0530

    Added:-result in Ascending order

--- sorting_Arthematic.sh
+++ sorting_Arthematic.sh
@@ -54,3 +54,24 @@ function sorting_Array_inDecending_order()
 }
 res="$( sorting_Array_inDecending_order ${arr[@]} )"
 echo "decending order is :$res"
+
+function sorting_Array_inAscending_order()
+{
+   for (( i=0; i<4; i++ ))
+   do
+        for (( j=$i+1; j<4; j++ ))
+        do
+             if [[ ${arr[i]} -gt ${arr[j]} ]]
+             then                                                                                                                                                                                                                    temp=${arr[i]}
+                  arr[i]=${arr[j]}
+                  arr[j]=$temp
+             fi
+        done
+   done
+              echo ${arr[@]}
+}
+res1="$( sorting_Array_inAscending_order ${arr[@]} )"
+echo "Ascending order is :$res1"
+
+
+
